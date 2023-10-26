d87b22e5361ba6591f083d4d0e0c1a886501e40c
Tabea <stoerbrock.tabea@gmail.com>
2023-10-26 13:06:21 +0200

    commit

--- calc.py
+++ calc.py
@@ -1,6 +1,17 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+while True : 
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Choose mathematical operation 'add' or 'multiply' : ")
+    choice = str(input())
+    if choice == "add" : 
+        print("Sum: " + str(num1+num2))
+    if choice == "multiply" : 
+        print("Product: " + str(num1*num2))
+
+    print("Programm beenden? Type 'y' or 'n': ")
+    exitCmd = str(input())
+    if exitCmd == "y" :
+        exit()
+
