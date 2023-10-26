6f4b7a45514fa07d9b2fba47c107d807d2c9cb63
Jago- <Jago-@192.168.2.163>
2023-10-26 13:23:50 +0200

    Aufgabe fertig

--- calc.py
+++ calc.py
@@ -1,6 +1,17 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+
+while True:
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Enter math operation")
+    operation = input()
+    if operation.casefold() == "add" or operation.casefold() == "addition":
+        print("Sum: " + str(num1+num2))
+    if operation.casefold() == "multi" or operation.casefold() == "multiplication":
+        print("Product: " + str(num1*num2))
+        
+    print ("w fuer weiter und e für exit")
+    end = input()
+    if end == "e":
+        exit()
