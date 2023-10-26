3d6e05aae8bddabcaef6eaa5531500a3489f978b
thelarsjedi <lmocka@smail.uni-koeln.de>
2023-10-26 13:21:28 +0200

    juhu es funktioniert

--- calc.py
+++ calc.py
@@ -1,6 +1,22 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+while True:
+
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Enter Math Operation:")
+    math1 = input()
+    if  math1 == "addition" or math1 =="add":
+        print("Sum: " + str(num1+num2))
+    if math1 == "multiply" or math1 == "multi":
+        print("Product: " + str(num1*num2))
+    print("Wanna continue (type 'yes' or 'y') or exit (type 'exit' or 'e')?")
+    userinput = input()
+    if userinput == "yes" or userinput =="y":
+        print("here we go again")
+        
+
+    if userinput == "exit" or userinput == "e":
+        print("see you later")
+        exit()
+    
