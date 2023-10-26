ff49f5c51c467647bd253b2c70fa594b8b130a72
mhrka <mhrkac@smail.uni-koeln.de>
2023-10-26 22:46:37 +0200

    erste funktionierende Version

--- calc.py
+++ calc.py
@@ -1,6 +1,16 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+x = True
+while x:
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Enter function: addition  or multiplication")
+    func = str(input())
+    if func == "add" :
+        print("Sum: " + str(num1+num2))
+    if func == "mult" :
+        print("Product: " + str(num1*num2))
+    print("Do you want to continue?")
+    y = str(input())
+    if y == "" :
+        break
