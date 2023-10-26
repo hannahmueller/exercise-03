04dbd48e1facd2514cb7c3fe75f0bd79cc23102f
Mw1701 <mwerk1@smail.uni-koeln.de>
2023-10-26 13:04:40 +0200

    excersie 2 done

--- calc.py
+++ calc.py
@@ -1,6 +1,28 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+con1 = "y"
+while con1=="y":
+
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+
+    print("Enter operation: ") #add or multiply
+    op1 = str(input())
+
+    # add/addition
+    if op1 == "add" : 
+        print("Sum: " + str(num1+num2))
+    if op1 == "addition" : 
+        print("Sum: " + str(num1+num2))
+
+    # multi/multiplication
+    if op1 == "multiply" : 
+        print("Product: " + str(num1*num2))
+    if op1 == "multiplication" : 
+        print("Product: " + str(num1*num2))
+
+
+    print("Continue? [y/n]: ") #start over?
+    con1 = str(input())  #y/n
+    if con1 == "y":
+        print()
