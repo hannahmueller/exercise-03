1fb263b061ff13dac66cf470831885e968d86419
lwechsl1 <l-wechsler@protonmail.com>
2023-10-26 13:13:53 +0200

    commit

--- calc.py
+++ calc.py
@@ -1,6 +1,22 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+x = True
+while x:
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print('commands: multi,add ')
+    func = str(input())
+    if(func == 'multi'):
+        res = num1 * num2
+        print('result', res)
+    elif(func == 'add'):
+        res = num1 + num2
+        print('result', res)
+    else:
+        print('non valid')
+
+    print('command for continue c')
+    resume = str(input())
+    if not (resume == 'c'):
+        x = False
+
