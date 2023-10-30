0bea223720394b224e99ea1ca696d1b0bd44d55e
hannahmueller <hmuell52@smail.uni-koeln.de>
2023-10-30 16:56:37 +0100

    Bearbeitet

--- calc.py
+++ calc.py
@@ -2,5 +2,25 @@ print("Enter number 1: ")
 num1 = int(input())
 print("Enter number 2: ")
 num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+print("Enter operation: ")
+op = str(input())
+if op == "add" or op == "+":
+    print("Sum: " + str(num1+num2))
+if op == "multi" or op == "*":
+    print("Product: " + str(num1*num2))
+print("If you would like to continue, please enter the letter y. If not, n. ")
+c = str(input())
+while (c == "y"):
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Enter operation: ")
+    op = str(input())
+    if op == "add" or op == "+":
+        print("Sum: " + str(num1+num2))
+    if op == "multi" or op == "/":
+        print("Product: " + str(num1*num2))
+    print("If you would like to continue, please enter the letter y. If not, n. ")
+    c = str(input())
+        
